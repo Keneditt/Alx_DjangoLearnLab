ec3744c04cd9257383f640f8fc89affc419a0b54
Kutlwano Mhlongo <kutlwanomakhalemele@gmail.com>
2025-09-12 11:19:01 +0200

    added Library

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,11 @@
 
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Library
 
 def list_books(request):
     """Function-based view to list all books"""
-    books = Book.objects.all()
+    books = Library.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):

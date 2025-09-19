5ef3d80749d345dad0e134bb77940f733f47458b
Kutlwano Mhlongo <kutlwanomakhalemele@gmail.com>
2025-09-19 10:43:10 +0200

    added line 13 on urls on relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,3 +8,14 @@ urlpatterns = [
     # Class-based view URL
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
 ]
+
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
+urlpatterns = [
+    # Other URL patterns for your app
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]

9b7ec5d427b3baf0a490b716cf438301dede464f
Kutlwano Mhlongo <kutlwanomakhalemele@gmail.com>
2025-09-29 09:50:21 +0200

    added code in model.py in blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    """
+    Blog Post model representing individual blog posts.
+    """
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+    
+    class Meta:
+        ordering = ['-published_date']

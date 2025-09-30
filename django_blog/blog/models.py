f7da91bc76f378e0b9c8b43ee279120947cd1cee
Kutlwano Mhlongo <kutlwanomakhalemele@gmail.com>
2025-09-30 11:11:42 +0200

    added code

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,12 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils import timezone
 
 class Post(models.Model):
+    """
+    Blog Post model representing individual blog posts.
+    """
     title = models.CharField(max_length=200)
     content = models.TextField()
-    # ForeignKey to Django's built-in User model
+    published_date = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    published_date = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+    
+    def save(self, *args, **kwargs):
+        """Set published_date to now if it's the first time being saved"""
+        if not self.published_date:
+            self.published_date = timezone.now()
+        super().save(*args, **kwargs)
+    
+    class Meta:
+        ordering = ['-published_date']
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,107 +1,165 @@
-# blog/views.py
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.views.generic import (
-    ListView,
-    DetailView,
-    CreateView,
-    UpdateView,
-    DeleteView
+    ListView, DetailView, CreateView, UpdateView, DeleteView
 )
-from django.urls import reverse_lazy
-from .forms import UserRegisterForm, UserUpdateForm, PostForm
-from .models import Post
+from django.contrib.auth.models import User
+from django.db.models import Q
+from .models import Post, UserProfile
+from .forms import PostForm, CustomUserCreationForm, UserUpdateForm, ProfileUpdateForm
 
-# --- Existing Authentication Views (register, profile) ---
-# ... (Keep existing register and profile views) ...
-
-# -------------------- CRUD VIEWS -----------------------
-
-# Replaces the simple post_list function
 class PostListView(ListView):
-    """Displays a list of all blog posts."""
+    """
+    Display a list of all blog posts with pagination and search functionality.
+    Accessible to all users (authenticated and anonymous).
+    """
     model = Post
-    template_name = 'blog/post_list.html'  # <app>/<model>_list.html
+    template_name = 'blog/post_list.html'
     context_object_name = 'posts'
+    paginate_by = 5
     ordering = ['-published_date']
-    paginate_by = 10
+    
+    def get_queryset(self):
+        """Add search functionality to the post list"""
+        queryset = super().get_queryset()
+        search_query = self.request.GET.get('search', '')
+        
+        if search_query:
+            queryset = queryset.filter(
+                Q(title__icontains=search_query) |
+                Q(content__icontains=search_query) |
+                Q(author__username__icontains=search_query)
+            )
+        
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        """Add additional context data"""
+        context = super().get_context_data(**kwargs)
+        context['search_query'] = self.request.GET.get('search', '')
+        context['total_posts'] = Post.objects.count()
+        return context
+
+class UserPostListView(ListView):
+    """
+    Display posts by a specific user.
+    """
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+    
+    def get_queryset(self):
+        """Get posts for the specific user"""
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-published_date')
+    
+    def get_context_data(self, **kwargs):
+        """Add user to context"""
+        context = super().get_context_data(**kwargs)
+        context['profile_user'] = get_object_or_404(User, username=self.kwargs.get('username'))
+        return context
 
 class PostDetailView(DetailView):
-    """Displays a single blog post."""
+    """
+    Display a single blog post in full detail.
+    Accessible to all users.
+    """
     model = Post
     template_name = 'blog/post_detail.html'
+    
+    def get_context_data(self, **kwargs):
+        """Add additional context for the post detail view"""
+        context = super().get_context_data(**kwargs)
+        post = self.get_object()
+        
+        # Get previous and next posts for navigation
+        context['previous_post'] = Post.objects.filter(
+            published_date__lt=post.published_date
+        ).order_by('-published_date').first()
+        
+        context['next_post'] = Post.objects.filter(
+            published_date__gt=post.published_date
+        ).order_by('published_date').first()
+        
+        return context
 
 class PostCreateView(LoginRequiredMixin, CreateView):
-    """Allows authenticated users to create a new post."""
-    model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
-    # Redirect to the detail view of the new post upon success
-    success_url = reverse_lazy('post-list')
-
-    def form_valid(self, form):
-        # Automatically set the author to the currently logged-in user
-        form.instance.author = self.request.user
-        messages.success(self.request, "Your post has been created!")
-        return super().form_valid(form)
-
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    """Allows the post author to update their post."""
+    """
+    Allow authenticated users to create new blog posts.
+    The author is automatically set to the current user.
+    """
     model = Post
     form_class = PostForm
     template_name = 'blog/post_form.html'
     
     def form_valid(self, form):
-        # Ensure the author field remains the same (security)
+        """Set the author to the current user before saving"""
         form.instance.author = self.request.user
-        messages.info(self.request, "Your post has been updated!")
+        messages.success(
+            self.request, 
+            'Your post has been created successfully!'
+        )
         return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        """Add context for the create view"""
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Create New Post'
+        context['submit_text'] = 'Create Post'
+        return context
 
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """
+    Allow post authors to update their existing posts.
+    Only the author can edit their own posts.
+    """
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    
+    def form_valid(self, form):
+        """Set success message on update"""
+        messages.success(
+            self.request, 
+            'Your post has been updated successfully!'
+        )
+        return super().form_valid(form)
+    
     def test_func(self):
-        # Custom test: only the post author can update it
+        """Ensure only the author can update the post"""
         post = self.get_object()
         return self.request.user == post.author
+    
+    def get_context_data(self, **kwargs):
+        """Add context for the update view"""
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Update Post'
+        context['submit_text'] = 'Update Post'
+        return context
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    """Allows the post author to delete their post."""
+    """
+    Allow post authors to delete their posts.
+    Only the author can delete their own posts.
+    """
     model = Post
     template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('post-list') # Redirect to the post list after deletion
-
-    def delete(self, request, *args, **kwargs):
-        messages.error(request, "Your post has been successfully deleted.")
-        return super().delete(request, *args, **kwargs)
-
+    success_url = '/'
+    
     def test_func(self):
-        # Custom test: only the post author can delete it
+        """Ensure only the author can delete the post"""
         post = self.get_object()
         return self.request.user == post.author
-
-# --- Existing simple function view (Replaced, but kept here for context) ---
-# def post_list(request):
-#     # This function is now superseded by PostListView
-#     return render(request, 'blog/index.html', {})
-
-# blog/views.py
-
-class PostListView(ListView):
-    # ...
-    template_name = 'blog/post_list.html'  # Matches file post_list.html
-
-class PostDetailView(DetailView):
-    # ...
-    template_name = 'blog/post_detail.html' # Matches file post_detail.html
-
-class PostCreateView(LoginRequiredMixin, CreateView):
-    # ...
-    template_name = 'blog/post_form.html' # Matches file post_form.html
-
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    # ...
-    template_name = 'blog/post_form.html' # Matches file post_form.html
     
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    # ...
-    template_name = 'blog/post_confirm_delete.html' # Matches file post_confirm_delete.html
+    def delete(self, request, *args, **kwargs):
+        """Set success message on delete"""
+        messages.success(
+            request, 
+            'Your post has been deleted successfully!'
+        )
+        return super().delete(request, *args, **kwargs)
+
+# ... (keep existing profile and registration views) ...
